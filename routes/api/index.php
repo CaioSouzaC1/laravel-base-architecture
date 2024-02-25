d6d054215c4c6523eb81e6033c63bd15b51e1c8d
CaioSouzaC1 <caiocsouza2010@gmail.com>
2024-02-25 19:10:22 -0300

    <CHORE>[Websockets]: Adding basic implementation

--- routes/api/index.php
+++ routes/api/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Builder\ReturnApi;
+use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('/auth')->group(base_path('routes/api/auth.php'));
-Route::post("/teste", function () {
-    echo "foi";
-    return true;
+Route::get("/", function () {
+    return ReturnApi::success(["running" => true], "Running");
 });
